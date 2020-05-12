7b5b853b4408033a40a82817303488bfc39b388f
StevenW1999 <0932549@hr.nl>
2020-05-12 15:58:28 +0200

    first commit

---
+++ src/Main.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+ 
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1>Menu balk</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/menu">Menu</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/menu" component={Menu}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+        </HashRouter>
+    );
+  }
+}
+ 
+export default Main;
---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>CONTACT INFO</h2>
+        <p>adress
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
