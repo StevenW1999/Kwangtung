bf035e6772aef5277da877d94f2c60cf0920e2f1
StevenW1999 <0932549@hr.nl>
2020-05-13 19:48:07 +0200

    background image added

--- src/Main.js
+++ src/Main.js
@@ -11,22 +11,8 @@ import Contact from "./pages/Contact";
 class Main extends Component {
   render() {
     return (
-      <div className="bg">
-      <HashRouter>
         <div>
-          <h1>Menu balk</h1>
-          <ul className="header">
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/menu">Menu</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-          </ul>
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/menu" component={Menu}/>
-            <Route path="/contact" component={Contact}/>
-          </div>
-        </div>
-        </HashRouter>
+          <div className="home"></div>
         </div>
     );
   }
